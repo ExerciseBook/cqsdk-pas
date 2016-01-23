4e8711ef06c00a648f3095315089325063cf5b18
Eric_Lian <505311335@qq.com>
2016-01-23 10:51:02 +0800

    Revert "008"
    This reverts commit 016352c07b99368fe3b7533662cef369e9567ba2.

--- lib/CoolQ_Function.pas
+++ lib/CoolQ_Function.pas
@@ -27,24 +27,7 @@ function CQ_setGroupLeave(AuthCode:longint;groupid:int64;isdismiss:boolean):long
 	stdcall; external 'CQP.dll' name 'CQ_setGroupLeave';
 function CQ_setGroupSpecialTitle(AuthCode:longint;groupid,QQID:int64;newspecialtitle:Pchar;duration:int64):longint;
 	stdcall; external 'CQP.dll' name 'CQ_setGroupSpecialTitle';
-function CQ_setDiscussLeave(AuthCode:longint;discussid:int64):longint;
-	stdcall; external 'CQP.dll' name 'CQ_setDiscussLeave';
-function CQ_setFriendAddRequest(AuthCode:longint;const responseflag:pchar;responseoperation:longint;const remark:Pchar):longint;
-	stdcall; external 'CQP.dll' name 'CQ_setFriendAddRequest';
-function CQ_setGroupAddRequestV2(AuthCode:longint;const responseflag:Pchar;requesttype,responseoperation:longint;const reason:Pchar):longint;
-	stdcall; external 'CQP.dll' name 'CQ_setGroupAddRequestV2';
 
-{
-CQAPI(const char *) CQ_getGroupMemberInfoV2(int32_t AuthCode, int64_t groupid, int64_t QQID, CQBOOL nocache);
-CQAPI(const char *) CQ_getStrangerInfo(int32_t AuthCode, int64_t QQID, CQBOOL nocache);
-CQAPI(int32_t) CQ_addLog(int32_t AuthCode, int32_t priority, const char *category, const char *content);
-CQAPI(const char *) CQ_getCookies(int32_t AuthCode);
-CQAPI(int32_t) CQ_getCsrfToken(int32_t AuthCode);
-CQAPI(int64_t) CQ_getLoginQQ(int32_t AuthCode);
-CQAPI(const char *) CQ_getLoginNick(int32_t AuthCode);
-CQAPI(const char *) CQ_getAppDirectory(int32_t AuthCode);
-CQAPI(int32_t) CQ_setFatal(int32_t AuthCode, const char *errorinfo);
-}
 {还有一堆在后面}
 	
 {调用简化}
